d42f375e165537ada4dfbe142f7375684a10a7c0
SergiiSt <119351759+SergiiSt@users.noreply.github.com>
2024-04-20 22:08:20 +0000

    Deploying to gh-pages from @ SergiiSt/goit-js-hw-09@13e4ae458a1c4f9e30d8c9cc9e0d9b8a8f788339 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector('input[name="email"]');function t(){document.activeElement===e?e.setAttribute("placeholder","Type area"):e.removeAttribute("placeholder")}e.addEventListener("focus",t);e.addEventListener("blur",t);
+//# sourceMappingURL=commonHelpers2.js.map
