669dccbe084dd5a80e79c4889280d4c577b5692d
SergiiSt <serjerl1991@gmail.com>
2024-04-21 14:38:56 +0200

    add js 2task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,55 @@
-const emailInput = document.querySelector('input[name="email"]');
-function setPlaceHolder() {
-    if (document.activeElement === emailInput) {
-        emailInput.setAttribute('placeholder', 'Type area');
-    }
-    else {
-        emailInput.removeAttribute('placeholder');
+const formEl = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+function loadFromLocal() {
+
+    const savedLocal = localStorage.getItem(localStorageKey);
+
+     if (savedLocal) {
+        try {
+           
+            const parsedData = JSON.parse(savedLocal);
+
+          if (parsedData && typeof parsedData === 'object') {
+            
+                formEl.elements.email.value = (parsedData.email || '').trim();
+                formEl.elements.message.value = (parsedData.message || '').trim();
+            } else {
+                console.error('Data in localStorage is not a valid JSON object.');
+            }
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
     }
 }
 
-emailInput.addEventListener('focus', setPlaceHolder);
-emailInput.addEventListener('blur', setPlaceHolder);
+  function saveToLocal() {
+
+const toLocal = {
+        email: formEl.elements.email.value.trim(),
+        message: formEl.elements.message.value.trim()
+    };
+    
+ localStorage.setItem(localStorageKey, JSON.stringify(toLocal));
+    return toLocal;
+}
+function pushedSubmit(evt) {
+      evt.preventDefault();
+    const savedData = saveToLocal();
+    if (!savedData.email || !savedData.message) {
+        alert('Всі поля повинні бути заповнені!');
+        return
+    }
+    console.log(savedData);
+    localStorage.clear();
+    formEl.elements.email.value = '';
+    formEl.elements.message.value = '';
+}
+
+loadFromLocal();
+
+formEl.addEventListener('input', saveToLocal);
+formEl.addEventListener('submit', pushedSubmit)
+
+
+
