b7e667e08170366ad14eeb906f2c0a14e08c9ea6
SergiiSt <serjerl1991@gmail.com>
2024-04-21 14:48:56 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,54 +2,47 @@ const formEl = document.querySelector('.feedback-form');
 const localStorageKey = 'feedback-form-state';
 
 function loadFromLocal() {
+  const savedLocal = localStorage.getItem(localStorageKey);
 
-    const savedLocal = localStorage.getItem(localStorageKey);
+  if (savedLocal) {
+    try {
+      const parsedData = JSON.parse(savedLocal);
 
-     if (savedLocal) {
-        try {
-           
-            const parsedData = JSON.parse(savedLocal);
-
-          if (parsedData && typeof parsedData === 'object') {
-            
-                formEl.elements.email.value = (parsedData.email || '').trim();
-                formEl.elements.message.value = (parsedData.message || '').trim();
-            } else {
-                console.error('Data in localStorage is not a valid JSON object.');
-            }
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-        }
+      if (parsedData && typeof parsedData === 'object') {
+        formEl.elements.email.value = (parsedData.email || '').trim();
+        formEl.elements.message.value = (parsedData.message || '').trim();
+      } else {
+        console.error('Data in localStorage is not a valid JSON object.');
+      }
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
     }
+  }
 }
 
-  function saveToLocal() {
+function saveToLocal() {
+  const toLocal = {
+    email: formEl.elements.email.value.trim(),
+    message: formEl.elements.message.value.trim(),
+  };
 
-const toLocal = {
-        email: formEl.elements.email.value.trim(),
-        message: formEl.elements.message.value.trim()
-    };
-    
- localStorage.setItem(localStorageKey, JSON.stringify(toLocal));
-    return toLocal;
+  localStorage.setItem(localStorageKey, JSON.stringify(toLocal));
+  return toLocal;
 }
 function pushedSubmit(evt) {
-      evt.preventDefault();
-    const savedData = saveToLocal();
-    if (!savedData.email || !savedData.message) {
-        alert('Всі поля повинні бути заповнені!');
-        return
-    }
-    console.log(savedData);
-    localStorage.clear();
-    formEl.elements.email.value = '';
-    formEl.elements.message.value = '';
+  evt.preventDefault();
+  const savedData = saveToLocal();
+  if (!savedData.email || !savedData.message) {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+  console.log(savedData);
+  localStorage.clear();
+  formEl.elements.email.value = '';
+  formEl.elements.message.value = '';
 }
 
 loadFromLocal();
 
 formEl.addEventListener('input', saveToLocal);
-formEl.addEventListener('submit', pushedSubmit)
-
-
-
+formEl.addEventListener('submit', pushedSubmit);
